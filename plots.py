4e81c28bdb576100241faf04175ee70734a2a536
Félix Martins <felix.marcial@hotmail.com>
2024-12-29 17:58:41 +0000

    Finished plot setup, moved results files
    Also added plot files

--- plots.py
+++ plots.py
@@ -2,12 +2,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 from typing import Optional
+import os
 
 def plot_files(files, title: Optional[str] = None):
     dfs = []
     file_label_column = "Approach"
     for file_label, file in files.items():
-        df = pd.read_csv(file)
+        df = pd.read_csv(os.path.join("results", file))
         df = df.agg(["mean", "std"], axis=0)
         df = df.transpose().reset_index()
         df.columns = ["Size", "Mean", "Std"]
@@ -15,7 +16,7 @@ def plot_files(files, title: Optional[str] = None):
         dfs.append(df)
 
     df = pd.concat(dfs)
-    df["Size"] = df["Size"].str[5:]
+    df["Size"] = df["Size"].str[5:].astype(int)
     sns.lineplot(x='Size', y='Mean', hue='Approach', data=df, marker='o')
     plt.title(title)
     plt.xlabel("Board Size (n x n)")
@@ -23,17 +24,17 @@ def plot_files(files, title: Optional[str] = None):
     plt.show()
 
 all_solutions_files = {
-    "Backtracking efficient": "results_backtracking_all_solutions.csv",
-    "Backtracking bitwise": "results_bitwise_all_solutions.csv",
-    # "OR-Tools": "results_or_tools_all_solutions.csv",
-    # "SAT Solver": "results_sat_all_solutions.csv",
+    "Backtracking Normal": "results_backtracking_all_solutions.csv",
+    "Backtracking Bitwise": "results_bitwise_all_solutions.csv",
+    "OR-Tools": "results_or_tools_all_solutions.csv",
+    "Glucose 4.2 SAT Solver": "results_sat_all_solutions.csv",
 }
 one_solution_files = {
+    "Backtracking Normal": "results_backtracking_one_solution.csv",
+    "Backtracking Bitwise": "results_bitwise_one_solution.csv",
     "OR-Tools": "results_or_tools_one_solution.csv",
-    "SAT Solver": "results_sat_one_solution.csv",
-    # "Backtracking efficient": "results_backtracking_one_solution.csv",
-    "Backtracking bitwise": "results_bitwise_one_solution.csv",
+    "Glucose 4.2 SAT Solver": "results_sat_one_solution.csv",
 }
 
-plot_files(all_solutions_files, title="Time taken to find ALL solutions (s)")
-plot_files(one_solution_files, title="Time taken to find just ONE solution (s)")
+plot_files(all_solutions_files, title="Time taken to find ALL solutions")
+plot_files(one_solution_files, title="Time taken to find just ONE solution")
