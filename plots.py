4db12107d8380f0feff0eb5b628471f55b9f7807
Félix Martins <felix.marcial@hotmail.com>
2024-12-29 15:35:03 +0000

    Added plots for finding just one solution

--- plots.py
+++ plots.py
@@ -1,28 +1,39 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+from typing import Optional
 
-result_files = {
-    # "Backtracking inefficient": "results_inefficient_backtracking",
-    "Backtracking efficient": "results_backtracking_all-solutions.csv",
-    "Backtracking bitwise": "results_bitwise_all-solutions.csv",
-    # "OR-Tools": "results_or_tools.csv",
-    # "SAT Solver": "results_sat.csv",
+def plot_files(files, title: Optional[str] = None):
+    dfs = []
+    file_label_column = "Approach"
+    for file_label, file in files.items():
+        df = pd.read_csv(file)
+        df = df.agg(["mean", "std"], axis=0)
+        df = df.transpose().reset_index()
+        df.columns = ["Size", "Mean", "Std"]
+        df[file_label_column] = file_label
+        dfs.append(df)
+
+    df = pd.concat(dfs)
+    df["Size"] = df["Size"].str[5:]
+    sns.lineplot(x='Size', y='Mean', hue='Approach', data=df, marker='o')
+    plt.title(title)
+    plt.xlabel("Board Size (n x n)")
+    plt.ylabel("Average Time Taken (s)")
+    plt.show()
+
+all_solutions_files = {
+    "Backtracking efficient": "results_backtracking_all_solutions.csv",
+    "Backtracking bitwise": "results_bitwise_all_solutions.csv",
+    # "OR-Tools": "results_or_tools_all_solutions.csv",
+    # "SAT Solver": "results_sat_all_solutions.csv",
+}
+one_solution_files = {
+    "OR-Tools": "results_or_tools_one_solution.csv",
+    "SAT Solver": "results_sat_one_solution.csv",
+    # "Backtracking efficient": "results_backtracking_one_solution.csv",
+    "Backtracking bitwise": "results_bitwise_one_solution.csv",
 }
 
-dfs = []
-file_label_column = "Approach"
-for file_label, file in result_files.items():
-    df = pd.read_csv(file)
-    df = df.agg(["mean", "std"], axis=0)
-    df = df.transpose().reset_index()
-    df.columns = ["Size", "Mean", "Std"]
-    df[file_label_column] = file_label
-    dfs.append(df)
-
-df = pd.concat(dfs)
-df["Size"] = df["Size"].str[5:]
-sns.lineplot(x='Size', y='Mean', hue='Approach', data=df, marker='o')
-plt.xlabel("Board Size (n x n)")
-plt.ylabel("Average Time Taken (s)")
-plt.show()
+plot_files(all_solutions_files, title="Time taken to find ALL solutions (s)")
+plot_files(one_solution_files, title="Time taken to find just ONE solution (s)")
